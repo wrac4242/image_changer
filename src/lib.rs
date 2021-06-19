083a6b340fb0d5facfb37ce05549cee2b1f8cfee
wrac4242 <wrac4242@gmail.com>
2021-06-19 13:02:45 +0100

    add Img struct, add Img new function

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,33 @@
-fn main() {
-    println!("Hello, world!");
+extern crate image;
+
+use image::{GenericImageView, DynamicImage};
+use image::io::Reader as ImageReader;
+use image::error::ImageError;
+use std::path::Path;
+use std::io;
+
+pub struct Img {
+    image: DynamicImage,
+}
+
+impl Img {
+    pub fn new(in_file: &Path) -> Result<Img, ImageError> {
+        let img: DynamicImage;
+
+        img = ImageReader::open(in_file)?.decode()?;
+
+        Ok(Img {
+            image: img,
+        })
+    }
+
+    pub fn to_file(&self, out_path: &Path) -> io::Result<()> {
+
+        Ok(())
+    }
+}
+
+// takes in a closure and applies it for each pixel
+fn filter_on_each_pixel() -> Img {
+    todo!()
 }
