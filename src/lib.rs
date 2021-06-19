5ae7ad46c28f59fd80bd530140f8d0932e86a92f
wrac4242 <wrac4242@gmail.com>
2021-06-19 15:29:08 +0100

    get saving and opening code working

--- src/lib.rs
+++ src/lib.rs
@@ -1,33 +1,49 @@
-extern crate image;
+#[allow(dead_code)]
+pub mod img {
+    extern crate image;
 
-use image::{GenericImageView, DynamicImage};
-use image::io::Reader as ImageReader;
-use image::error::ImageError;
-use std::path::Path;
-use std::io;
+    use image::{GenericImageView, DynamicImage};
+    use image::io::Reader as ImageReader;
+    use std::path::Path;
+    use std::error;
+    use crate::utils::outer::paths;
 
-pub struct Img {
-    image: DynamicImage,
-}
+    // Change the alias to `Box<error::Error>`.
+    type Result<T> = std::result::Result<T, Box<dyn error::Error>>;
 
-impl Img {
-    pub fn new(in_file: &Path) -> Result<Img, ImageError> {
-        let img: DynamicImage;
-
-        img = ImageReader::open(in_file)?.decode()?;
-
-        Ok(Img {
-            image: img,
-        })
+    pub struct Img {
+        image: DynamicImage,
     }
 
-    pub fn to_file(&self, out_path: &Path) -> Result<(), ImageError> {
-        &self.image.save(out_path)?;
-        Ok(())
+    impl Img {
+        pub fn new(in_file: &Path) -> Result<Img> {
+            let img: DynamicImage;
+
+            let file_path = match paths::absolute_path(in_file) {
+                Ok(e) => e,
+                Err(a) => panic!("Error: {:?}", a)
+            };
+            img = ImageReader::open(file_path.as_path())?.decode()?;
+
+            Ok(Img {
+                image: img,
+            })
+        }
+
+        pub fn to_file(&self, out_path: &Path) -> Result<()> {
+            let file_path = match paths::absolute_path(out_path) {
+                Ok(e) => e,
+                Err(a) => panic!("Error: {:?}", a)
+            };
+            &self.image.save(file_path.as_path())?;
+            Ok(())
+        }
+    }
+
+    // takes in a closure and applies it for each pixel
+    fn filter_on_each_pixel() -> Img {
+        todo!()
     }
 }
 
-// takes in a closure and applies it for each pixel
-fn filter_on_each_pixel() -> Img {
-    todo!()
-}
+pub(self) mod utils;
