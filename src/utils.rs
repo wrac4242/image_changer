4d0db250fd6c89de55fce6029462ca8eb835e018
wrac4242 <wrac4242@gmail.com>
2021-06-20 09:19:10 +0100

    add hashing function to utils

--- src/utils.rs
+++ src/utils.rs
@@ -3,6 +3,11 @@ use std::env;
 use std::io;
 use std::path::{PathBuf, Path};
 
+use data_encoding::HEXUPPER;
+use ring::digest::{Context, Digest, SHA256};
+use std::fs::File;
+use std::io::{BufReader, Read};
+
 use path_clean::PathClean;
 
 pub fn absolute_path(path: impl AsRef<Path>) -> io::Result<PathBuf> {
@@ -15,4 +20,29 @@ pub fn absolute_path(path: impl AsRef<Path>) -> io::Result<PathBuf> {
     }.clean();
 
     Ok(absolute_path)
+}
+
+// code from https://rust-lang-nursery.github.io/rust-cookbook/cryptography/hashing.html
+
+fn sha256_digest<R: Read>(mut reader: R) -> io::Result<Digest> {
+    let mut context = Context::new(&SHA256);
+    let mut buffer = [0; 1024];
+
+    loop {
+        let count = reader.read(&mut buffer)?;
+        if count == 0 {
+            break;
+        }
+        context.update(&buffer[..count]);
+    }
+
+    Ok(context.finish())
+}
+
+pub fn hash_file(path: &Path) -> io::Result<String> {
+    let input = File::open(path)?;
+    let reader = BufReader::new(input);
+    let digest = sha256_digest(reader)?;
+
+    Ok(HEXUPPER.encode(digest.as_ref()))
 }
